691700bd3eb3fefebbab0f04d39f41a47b431dc1
Owenkimcertified <ehdrb3309@naver.com>
2024-04-13 23:17:11 +0900

    24-04-13

--- cluster_producer.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaProducer
-
-brokers = ["localhost:9091", "localhost:9092", "localhost:9093"]
-topicname = "first-cluster-topic" 
-
-producer = KafkaProducer(bootstrap_servers = brokers)
-
-producer.send(topicname, b"hello first cluster")
-producer.flush() # buffer clear
